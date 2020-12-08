5a4dd17bad7172eafecde06e283afba00eca7e6f
GoshaFighten <goshafighten@gmail.com>
2020-12-08 20:43:05 +0300

    Build RichEdit

---
+++ RichText.Module.Blazor/gulpfile.js
@@ -0,0 +1,50 @@
+const { src, dest } = require('gulp');
+const { series } = require('gulp');
+const spawn = require('child_process').spawn;
+
+function installModules(cb) {
+  spawn('npm', ['install'], {
+    cwd: 'node_modules/devexpress-richedit/',
+    stdio: 'inherit',
+    shell: true,
+  }).on('close', function (code) {
+    cb(code);
+  });
+}
+function buildRich(cb) {
+  spawn('npm', ['run build-custom'], {
+    cwd: 'node_modules/devexpress-richedit/',
+    stdio: 'inherit',
+    shell: true,
+  }).on('close', function (code) {
+    cb(code);
+  });
+}
+function copy(source, destination) {
+  return src(source).pipe(dest(destination));
+}
+function copyRichJS() {
+  return copy(
+    'node_modules/devexpress-richedit/dist/custom/dx.richedit.min.js',
+    'wwwroot'
+  );
+}
+function copyRichCSS() {
+  return copy(
+    'node_modules/devexpress-richedit/dist/custom/dx.richedit.css',
+    'wwwroot'
+  );
+}
+function copyRichIcons() {
+  return copy(
+    'node_modules/devexpress-richedit/dist/custom/icons/**/*',
+    'wwwroot/icons'
+  );
+}
+exports.default = series(
+  installModules,
+  buildRich,
+  copyRichJS,
+  copyRichCSS,
+  copyRichIcons
+);
