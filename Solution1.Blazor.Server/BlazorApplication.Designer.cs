10ff474546a6f940afc0d563b0a5f88dbeda41bf
GoshaFighten <goshafighten@gmail.com>
2021-01-15 21:02:15 +0300

    Remove non-necessary projects

--- Solution1.Blazor.Server/BlazorApplication.Designer.cs
+++ Solution1.Blazor.Server/BlazorApplication.Designer.cs
@@ -26,7 +26,6 @@
             this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
             this.module2 = new DevExpress.ExpressApp.Blazor.SystemModule.SystemBlazorModule();
             this.module3 = new Solution1.Module.Solution1Module();
-            this.module4 = new Solution1.Module.Blazor.Solution1BlazorModule();
             this.securityModule1 = new DevExpress.ExpressApp.Security.SecurityModule();
             this.conditionalAppearanceModule = new DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule();
             this.validationModule = new DevExpress.ExpressApp.Validation.ValidationModule();
@@ -43,7 +42,6 @@
             this.Modules.Add(this.module1);
             this.Modules.Add(this.module2);
             this.Modules.Add(this.module3);
-            this.Modules.Add(this.module4);
             this.Modules.Add(this.securityModule1);
             this.Modules.Add(this.conditionalAppearanceModule);
             this.Modules.Add(this.validationModule);
@@ -60,7 +58,6 @@
         private DevExpress.ExpressApp.SystemModule.SystemModule module1;
         private DevExpress.ExpressApp.Blazor.SystemModule.SystemBlazorModule module2;
         private Solution1.Module.Solution1Module module3;
-        private Solution1.Module.Blazor.Solution1BlazorModule module4;
         private DevExpress.ExpressApp.Security.SecurityModule securityModule1;
         private DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule conditionalAppearanceModule;
         private DevExpress.ExpressApp.Validation.ValidationModule validationModule;
